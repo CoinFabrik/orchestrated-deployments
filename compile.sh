7a017218d654e5438358c5e34dc2c75bcae5d22e
Franco <scucchiero@gmail.com>
2019-03-26 15:50:33 -0300

    Updated used static for compiling.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 cd contracts
-./../solc-5.3 --abi --bin --overwrite --optimize --optimize-runs 0 -o ../build Proxy.sol
-./../solc-5.3 --abi --bin --overwrite --optimize --optimize-runs 0 -o ../build ERC20.sol
-./../solc-5.3 --abi --bin --overwrite --optimize --optimize-runs 0 -o ../build WonderfulERC20.sol
+./../solc-5.6 --abi --bin --overwrite --optimize --optimize-runs 0 -o ../build Proxy.sol
+./../solc-5.6 --abi --bin --overwrite --optimize --optimize-runs 0 -o ../build ERC20.sol
+./../solc-5.6 --abi --bin --overwrite --optimize --optimize-runs 0 -o ../build WonderfulERC20.sol
